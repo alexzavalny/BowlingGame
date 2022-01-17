e67b48bbdd79a42e4000ccb2da29925c949abf8c
alexzavalny <alex@zavalny.com>
2022-01-17 12:33:22 +0200

    bowling frame implementation

--- lib/bowling_frame.rb
+++ lib/bowling_frame.rb
@@ -4,20 +4,37 @@ class BowlingFrame
     @rolls = []
   end
 
-  # inserts roll into frame is possible
+  # inserts roll into frame if possible
   def add_roll(roll)
+    # TODO: add validation
+    @rolls << roll
+  end
+
+  def full?
+    if @frame_number < 10
+      strike? or @rolls.length == 2
+    else
+      @rolls.length == if strike? || spare?
+                         3
+                       else
+                         2
+                       end
+    end
   end
 
   # checks if first roll is 10
   def strike?
+    @rolls[0] == 10
   end
   
   # checks if two rolls sum is 10
   def spare?
+    @rolls.length > 1 && @rolls[0..1].sum == 10
   end
 
   # calculates total score for this frame
   # NOTE: this score is isolated from other frames
   def score 
+    @rolls.sum
   end
 end
